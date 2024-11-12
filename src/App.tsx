2a5caf0b7c1f304007a9e2dc4cb9639a736f5cad
PY-SENSEI <gauravjena48@gmail.com>
2024-11-12 23:52:45 +0530

    added frontend with shadcn

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <Button variant="default">Hello World</Button>
+    </h1>
+  )
+}
